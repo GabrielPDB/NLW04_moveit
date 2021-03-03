b66abde56d4cf7956dc324de0af9ce1c750185d2
GabrielPDB <39529079+GabrielPDB@users.noreply.github.com>
2021-03-02 22:40:55 -0300

    Experience bar adjust

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -12,7 +12,7 @@ export function ExperienceBar() {
         <header className={styles.experienceBar}>
             <span>0 xp</span>
             <div>
-                <div style={{ width: `${percentToNextLevel}%` }} />
+                <div style={{ width: `${currentExperience}%` }} />
 
                 <span className={styles.currentExperience} style={{ left: `${percentToNextLevel}%` }}>{currentExperience} xp</span>
             </div>
